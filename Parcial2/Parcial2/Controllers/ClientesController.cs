ca744f804f8edd3411a19c5f7d680be834484b68
SEBASTIAN HENAO LOPEZ <sebastian-henaol@unilibre.edu.co>
2018-10-12 10:29:11 -0500

    Profe termine el parcial

---
+++ Parcial2/Parcial2/Controllers/ClientesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Random()
+        {
+			var cliente = new Clientes()
+			{
+				Nombre = "Sebastian",
+				Apellido = "Henao",
+				Sueldo = 5000000
+			};
+            return View(cliente);
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+	public class Clientes
+	{
+		public int ID { get; set; }
+		public String Nombre { get; set; }
+		public String Apellido { get; set; }
+		public float Sueldo { get; set; }
+	}
+}
